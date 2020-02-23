00a2f885757a280061c9b68372b4de1185a2ba45
singuye <simon_nguyen@gap.com>
2020-02-23 11:46:38 -0800

    completed setup for webpack and babel 7 react boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # boilerplate_custom_webpack_babel_react_template
+
+## Follow this guide: https://hackernoon.com/how-to-setup-webpack-and-babel-7-for-react-6xpy3615
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      // Rules to load scss
+      {
+        test: /\.scss$/i, //regex - looking file ends with scss
+        use: [
+          "style-loader", //3 inject styles into dom
+          "css-loader", //2. turn css into commonjs
+          "sass-loader" //1. turn sass into css
+        ]
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
+};
