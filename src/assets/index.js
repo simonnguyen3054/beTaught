dbe287445c9a76ef00e3d3cc6cac2428216974bc
Simon Nguyen <simonnguyen3054@gmail.com>
2020-04-26 13:36:13 -0700

    added images to covers

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as image1 } from "./images/image1.jpg";
+export { default as image2 } from "./images/image2.jpg";
+export { default as image3 } from "./images/image3.jpg";
--- src/layouts/Profile/components/Cover/Cover.js
+++ src/layouts/Profile/components/Cover/Cover.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { Link as RouteLink } from "react-router-dom";
+import { image1, image2, image3 } from "../../../../assets";
+import "./style.scss";
 
 const Cover = () => {
-  return <div>Profile Cover</div>;
+  const highlights = [
+    {
+      url: image1,
+    },
+    { url: image2 },
+    {
+      url: image3,
+    },
+  ];
+  return (
+    <div className="highlights">
+      {highlights.map((image, i) => {
+        return <img key={i} src={image.url} className="highlights-image"/>;
+      })}
+    </div>
+  );
 };
 
 export default Cover;
