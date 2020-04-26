9022cf0c2043a9ac63775f67eea82526143e0081
Simon Nguyen <simonnguyen3054@gmail.com>
2020-04-26 15:19:38 -0700

    added profile image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 export { default as image1 } from "./images/image1.jpg";
 export { default as image2 } from "./images/image2.jpg";
 export { default as image3 } from "./images/image3.jpg";
+export { default as profile } from "./images/profile.jpg";
--- src/layouts/Profile/components/Cover/Cover.js
+++ src/layouts/Profile/components/Cover/Cover.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link as RouteLink } from "react-router-dom";
-import { image1, image2, image3 } from "../../../../assets";
+import { image1, image2, image3, profile } from "../../../../assets";
 import "./style.scss";
 
 const Cover = () => {
@@ -14,10 +14,17 @@ const Cover = () => {
     },
   ];
   return (
-    <div className="highlights">
-      {highlights.map((image, i) => {
-        return <img key={i} src={image.url} className="highlights-image"/>;
-      })}
+    <div>
+      <div className="highlights">
+        {highlights.map((image, i) => {
+          return <img key={i} src={image.url} className="highlights-image" />;
+        })}
+      </div>
+      <div className="profile">
+        <div className="profile-inner">
+          <img src={profile} />
+        </div>
+      </div>
     </div>
   );
 };
